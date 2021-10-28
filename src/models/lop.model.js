1d23af5eb35c42ec179bfbe7ec52f122168b183f
Man <88386240+man49116@users.noreply.github.com>
2021-10-28 11:30:46 +0700

    baitap

---
+++ src/models/lop.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { toJSON, paginate } = require('./plugins');
+const { roles } = require('../config/roles');
+
+const lopSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },id: {
+      type: String,
+      required: true,
+      trim: true,
+    }
+    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+lopSchema.plugin(toJSON);
+lopSchema.plugin(paginate);
+
+
+/**
+ * @typedef Lop
+ */
+const Lop = mongoose.model('Lop', lopSchema);
+
+module.exports = Lop;
